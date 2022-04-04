64589db4340445d95aa2055d32cfcf8d2521957a
Goat.St3 <stefano2301@live.it>
2022-04-04 16:08:51 +0200

    all exercises readded

---
+++ Es12/exercise-2.js
@@ -0,0 +1,8 @@
+const concat = function (arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+const concat1 = (arr1, arr2)=>{return arr1.concat1(arr2);}
+
+console.log(concat([2, 4], [5, 1]));
+console.log(concat1([2, 4], [5, 1]));

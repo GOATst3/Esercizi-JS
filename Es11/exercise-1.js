cf99b80118fb2d27f17ed68e6e35b68fd8cbbad1
Goat.St3 <stefano2301@live.it>
2022-04-04 15:59:11 +0200

    es11 done

---
+++ Es11/exercise-1.js
@@ -0,0 +1,14 @@
+const sum = function (a, b) {
+  return a + b;
+}
+
+const log = function (value) {
+  console.log(value);
+}
+
+
+const sum1 = (a,b)=> {return a+b;}
+const log1 = (value) =>{console.log(value);}
+
+log(sum(2, 5));
+log1(sum1(2,5));

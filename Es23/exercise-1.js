a5310a2587f9b3874ee8cc70ca33c548ce8d07d8
Goat.St3 <stefano2301@live.it>
2022-04-06 15:59:40 +0200

    es23 done

---
+++ Es23/exercise-1.js
@@ -0,0 +1,8 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+for (let i = 0; i < Object.keys(person).length; i++) {
+  console.log(Object.keys(person)[i]+": "+Object.values(person)[i]);
+}

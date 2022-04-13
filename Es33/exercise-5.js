a34e019e232d35087f400a2d7a0c96191d1cd7a5
Goat.St3 <stefano2301@live.it>
2022-04-13 18:06:46 +0200

    es33 done

---
+++ Es33/exercise-5.js
@@ -0,0 +1,31 @@
+function firstUncompletedNote(notes) {
+  return notes.find(item=>item.todos.some(todos=>!todos.done))
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      { id: 1, name: "Push ups - 10 x 3", done: false },
+      { id: 2, name: "Abdominals - 20 x 3", done: true },
+      { id: 3, name: "Tapis Roulant - 15min", done: true },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      { id: 1, name: "Learn HTML", done: true },
+      { id: 2, name: "Learn CSS", done: true },
+      { id: 3, name: "Learn JavaScript", done: true },
+      { id: 4, name: "Learn Angular", done: false },
+    ],
+  },
+];
+
+const noteInProgress = firstUncompletedNote(notes);
+console.log("All notes: ");
+console.dir(notes, {depth: null});
+console.log("Notes in progress: ");
+console.dir(noteInProgress,{depth: null});

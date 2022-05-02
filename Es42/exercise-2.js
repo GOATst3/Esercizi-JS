3540812a7d681ab7bf6b6a892a98a68962dd155b
Goat.St3 <stefano2301@live.it>
2022-05-02 16:07:13 +0200

    es 42 done

---
+++ Es42/exercise-2.js
@@ -0,0 +1,39 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    try{
+      if (amount < 0) throw new Error('The amount provided cannot be negative');
+      this.#amount += amount;
+    }
+    catch(err){
+      console.error(err);
+    }
+  }
+
+  withdraw(amount) {
+    try{
+      if (amount < 0) throw new Error('The amount provided cannot be negative');
+      else if (this.#amount < amount) throw new Error('You cannot withdraw more than account balance');
+      else this.#amount -= amount;
+    }
+    catch(err){
+      console.error(err);
+    }
+}
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+// Handle errors to avoid app crash
+const bankAccount = new BankAccount(1000);
+bankAccount.deposit(500);
+bankAccount.deposit(200);
+bankAccount.withdraw(10000);
+bankAccount.view();
